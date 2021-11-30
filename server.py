99cdba712f01617893c156b7c0dc7a0ac2c855ab
adrianWarrior <adrianoforcellinipedretti@gmail.com>
2021-11-30 08:59:05 -0300

    Configurada Conexao com MySQL

--- server.py
+++ server.py
@@ -1,6 +1,14 @@
 from flask import request, Flask, jsonify
-
+from flask_sqlalchemy import SQLAlchemy
+from SQLALCHEMY_DATABASE_URI import SQLALCHEMY_DATABASE_URI
 app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+# string SQLALCHEMY_DATABASE_URI = "mysql://'seu_usuario':'sua_senha'@localhost/'seu_db'"
+
+
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
@@ -17,3 +25,9 @@ def get_json():
 
 if __name__ == "__main__":
     app.run()
+
+
+class Example(db.Model):
+    __tablename__ = 'example'
+    id = db.Column('id', db.Integer, primary_key=True)
+    data = db.Column('data', db.Unicode)
